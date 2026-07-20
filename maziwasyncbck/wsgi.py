0c7fe91f8deef9d556c3386aac78ee6eb098fb02
jobndirangu <jobndirangu27@gmail.com>
2026-07-20 12:23:49 +0300

    First commit

--- maziwasyncbck/wsgi.py
+++ maziwasyncbck/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append("/home/milksync/www/maziwasyncbck")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maziwasyncbck.settings')
 
 application = get_wsgi_application()
